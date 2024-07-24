eb610c05534baa09717573d396a8c8ffabcc14bd
Lakshan Wijesekara <int.lakshan@creativesoftware.com>
2024-07-24 16:10:02 +0530

    Coded to fetch data from a json file
    * Make the json file and used httpClient method to call the data

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UserComponent } from './pages/user/user.component';
+import { provideHttpClient } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,10 @@ import { UserComponent } from './pages/user/user.component';
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    provideClientHydration(),
+    provideHttpClient()
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/pages/user/user.component.ts
+++ src/app/pages/user/user.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DataService } from './userdata.service';
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
-  styleUrl: './user.component.css'
+  styleUrl: './user.component.scss'
 })
-export class UserComponent {
-
+export class UserComponent implements OnInit{
+  data: any;
+  value: string = '';
+  constructor(private dataService: DataService){}
+  
+  ngOnInit(){
+  this.dataService.fetchData().subscribe(data=>{
+          this.data = data;
+      });
+  }
 }
