7a1c1eda033e5392117b79f0a77c3f41e0e4ec1c
Lakshan Wijesekara <int.lakshan@creativesoftware.com>
2024-10-08 17:03:41 +0530

    Modify code structure to update and view users
    * Add enum propertyVisibility to track the state of the selection
    * Define conditions to change the view accordingly in the HTML file
    * CSS adjustments

--- src/app/pages/add-property/add-property.component.ts
+++ src/app/pages/add-property/add-property.component.ts
@@ -10,7 +10,14 @@ import { City } from '../../interfaces/city';
 import { PropertydataService } from '../../services/propertydata.service';
 import { Property } from '../../interfaces/property';
 import { MessageService } from 'primeng/api';
+import { MarkerService } from '../../services/marker.service';
+import { Marker } from '../../interfaces/marker';
 
+//enum was used here to track the state of add property and update property
+enum propertyState {
+  AddProperty = 'addProperty',
+  UpdateProperty = 'updateProperty',
+}
 @Component({
   selector: 'app-properties',
   templateUrl: './add-property.component.html',
@@ -20,12 +27,17 @@ export class AddPropertyComponent implements OnInit {
   groupedCities: City[] = [];
   inputValue: string | undefined;
   isAddPropertyVisible: boolean = false;
-  isViewPropertyVisible: boolean = false;
-  dropdownSelectedCity: string = '';
+  dropdownSelectedCity: string | undefined;
   //Get the input from the property search box
   searchText: string = '';
+  selectedLocation!: Marker | undefined;
   selectedProperty!: Property | undefined;
-
+  propertyVisibility = propertyState;
+  propertyShowState: propertyState = propertyState.AddProperty;
+  id: number = 0;
+  defaultLatitude: number = 6.9271;
+  defaultLongtitude: number = 79.8612;
+  //Reactive form (selectedProperty is not defined since it is a stand-alone component)
   reactiveForm: FormGroup = new FormGroup({
     propertyName: new FormControl('', [Validators.required]),
     propertyArea: new FormControl('', [
@@ -76,19 +88,61 @@ export class AddPropertyComponent implements OnInit {
 
   //Show the pop-up
   showDialog(): void {
+    this.propertyShowState = this.propertyVisibility.AddProperty;
     this.isAddPropertyVisible = true;
+  }
+
+  selectedLatitude(): number {
+    let lat = this.selectedLocation?.lat || this.defaultLatitude;
+    return lat;
+  }
+
+  selectedLongtitude(): number {
+    let lng = this.selectedLocation?.lng || this.defaultLongtitude;
+    return lng;
+  }
+
+  selectedPropertyLatitude(): number {
+    this.propertyShowState = this.propertyVisibility.UpdateProperty;
+    let latitude =
+      this.selectedProperty?.latitude || this.selectedLocation!.lat || 0;
+    return latitude;
+  }
+
+  selectedPropertyLongtitude(): number {
+    this.propertyShowState = this.propertyVisibility.UpdateProperty;
+    let longtitude =
+      this.selectedProperty?.longtitude || this.selectedLocation?.lng || 0;
+    return longtitude;
+  }
+
+  //To update the property with a new value
+  updateProperty(propertyform: FormGroupDirective, id: number): void {
+    const updatedProperty = {
+      id: id,
+      selectedCity: this.dropdownSelectedCity!,
+      propertyName: propertyform.value.propertyName,
+      propertyArea: propertyform.value.propertyArea,
+      monthlyRental: propertyform.value.monthlyRental,
+    };
+    return this.propertyDataService.updateProperty(updatedProperty);
+  }
+
+  //View property will assign the values according to changes in the DOM
+  viewDialog(property: Property) {
+    this.propertyShowState = this.propertyVisibility.UpdateProperty;
+    this.selectedProperty = property;
+    this.isAddPropertyVisible = true;
+    this.id = property.id;
+    this.dropdownSelectedCity = property.selectedCity;
+    this.propertyName = property.propertyName;
+    this.propertyArea = property.propertyArea;
+    this.monthlyRental = property.monthlyRental;
   }
 
   //Close the pop-up
   closeDialog(): void {
     this.isAddPropertyVisible = false;
-    this.isViewPropertyVisible = false;
-  }
-
-  //Passing the property input parameter is a must, to correctly show the updated property in the pop-up map
-  viewProperty(property: Property) {
-    this.isViewPropertyVisible = true;
-    this.selectedProperty = property;
   }
 
   //When calling the property list, if there's a value in propertySearch box the filter runs
@@ -102,10 +156,6 @@ export class AddPropertyComponent implements OnInit {
             p.propertyName?.toLowerCase().includes(searchText.toLowerCase()) ||
             p.propertyArea?.toString().includes(searchText) ||
             p.monthlyRental?.toString().includes(searchText)
-            p.selectedCity?.toLowerCase().includes(searchText.toLowerCase()) ||
-            p.propertyName?.toLowerCase().includes(searchText.toLowerCase()) ||
-            p.propertyArea?.toString().includes(searchText) ||
-            p.monthlyRental?.toString().includes(searchText)
         );
       return property;
     }
@@ -113,20 +163,20 @@ export class AddPropertyComponent implements OnInit {
   }
 
   //The dropdownSelectedCity is passed to this method from the html and used to find the specific location
-  // getPropertyLocation(dropdownSelectedCity: string) {
-  //   this.markerService.getMarkerLocation(dropdownSelectedCity).subscribe({
-  //     next: (marker) => {
-  //       this.selectedPropertyLocation = marker!; //If correct
-  //     },
-  //     error: (error) => {
-  //       this.messageService.add({
-  //         severity: 'error',
-  //         summary: 'Error',
-  //         detail: 'Error occurred' + error,
-  //       });
-  //     },
-  //   });
-  // }
+  getPropertyLocation(dropdownSelectedCity: string | undefined) {
+    this.markerService.getMarkerLocation(dropdownSelectedCity!).subscribe({
+      next: (marker) => {
+        this.selectedLocation = marker!; //If correct default marker location
+      },
+      error: (error) => {
+        this.messageService.add({
+          severity: 'error',
+          summary: 'Error',
+          detail: 'Error occurred' + error,
+        });
+      },
+    });
+  }
 
   //Add property to the signal
   addProperty(propertyform: FormGroupDirective): void {
@@ -134,15 +184,12 @@ export class AddPropertyComponent implements OnInit {
     const newProperty = {
       id: this.propertyDataService.properties().length + 1,
       selectedCity: this.dropdownSelectedCity!,
-      id: this.propertyDataService.properties().length + 1,
-      selectedCity: this.dropdownSelectedCity!,
       propertyName: propertyform.value.propertyName,
       propertyArea: propertyform.value.propertyArea,
       monthlyRental: propertyform.value.monthlyRental,
-      // latitude: this.selectedPropertyLocation?.lat,
-      // longtitude: this.selectedPropertyLocation?.lng,
+      latitude: this.selectedLocation?.lat,
+      longtitude: this.selectedLocation?.lng,
     };
-
 
     const response = this.propertyDataService.addProperty(newProperty);
     if (response.status === 'success') {
@@ -174,17 +221,9 @@ export class AddPropertyComponent implements OnInit {
   //PRIVATE
 
   //Get properties from the properties JSON file
-  //Get properties from the properties JSON file
   private fetchProperties(): void {
     this.propertyDataService.fetchData().subscribe((propertyJSON) => {
       this.propertyDataService.properties.set(propertyJSON);
-    });
-  }
-
-  //Get cities from the cities JSON file
-  private getCities(): void {
-    this.cityDataService.fetchCityData().subscribe((groupedCities) => {
-      this.groupedCities = groupedCities;
     });
   }
 
